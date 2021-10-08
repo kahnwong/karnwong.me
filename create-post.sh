2d5df29c7fe6ad19be8999eff219dc94cdaab3f0
Karn Wong <karn.wong+github@mailbox.org>
2021-10-08 17:29:02 +0700

    add post

--- create-post.sh
+++ create-post.sh
@@ -1,9 +1,11 @@
-title="Title here"
+#!/bin/bash
+
+title="ecs-cli snippets"
 
 slug=$(echo "$title" | awk '{print tolower($0)}')
 slug=$(echo "$slug" | tr " " -)
 
-date=$(date "+%Y-%M-%dT%H:%M:%S%Z:00")
+date=$(date "+%Y-%m-%dT%H:%M:%S%Z:00")
 tag="sample-tag"
 
 echo "---
